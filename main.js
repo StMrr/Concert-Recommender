3b7cdfeaea8de5b9b14bf5149f0e7b86feb3b9ec
StMrr <boldeagle175@gmail.com>
2022-06-05 16:51:25 -0400

    finally got the spotify window to open

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 const dotenv = require('dotenv').config();
 const SpotifyWebApi = require('spotify-web-api-node');
 const electron = require('electron');
+const path = require('path');
+const {ipcMain} = require('electron');
+const helperFuncs = require('./helperFuncs.js')
 
 let ticketKey = process.env.TICKETMASTER_CONSUMER_KEY;
 
 let spotifyApi = new SpotifyWebApi({
   clientId: process.env.SPOTIFY_CLIENT_ID,
   clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-  redirectUri: 'https://localhost'
+  redirectUri: 'https://localhost:8000/callback'
 });
 let scope = ['user-top-read'],
   state = 'some-state';
@@ -18,88 +21,25 @@ let topArtistsList = [];
 function createMainWindow() {
   let mainWin = new electron.BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true
+      //preload: path.join(__dirname, 'helperFuncs.js')
+    }
   });
 
   mainWin.loadFile('index.html');
+  //mainWin.webContents.openDevTools();
 }
 
-function createAuthWindow() {
-  let authCode = '';
-  let authWindow = new electron.BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: false,
-      enableRemoteModule: false
-    },
-    show: false,
-  })
+ipcMain.on("btnclick", async (event, arg) => {
+  await helperFuncs.createAuthWindow();
+  console.log('testing');
+  event.sender.send("btnclick-task-finished", "yes");
+});
 
-  authWindow.loadURL(authorizeURL);
-  const {session : {webRequest}} = authWindow.webContents;
-  const filter = {
-    urls: [
-      'https://localhost/*'
-    ]
-  };
-  authWindow.show();
-  webRequest.onBeforeRequest(filter, function(details, callback) {
-    const url = details.url;
-    let firstIndex = url.indexOf('code=')+5;
-    let secondIndex = url.indexOf("&state")-6;
-    authCode = url.substr(firstIndex, secondIndex);
-    authCode = authCode.substr(0, authCode.indexOf("&state"));
-
-    callback({
-      cancel: false
-    });
-
-    getAccessToken(authCode);
-  });
-  authWindow.webContents.on('did-get-redirect-request', function(event, oldUrl, newUrl) {
-    console.log('Did it work?');
-    console.log(newUrl);
-  });
-
-  //authWindow.close();
-  authWindow.on('closed', function() {
-    authWindow = null;
-  });
-}
-
-async function getAccessToken(authCode){
-  spotifyApi.authorizationCodeGrant(authCode).then(
-    function(data) {
-     //console.log('The token expires in ' + data.body['expires_in']);
-     //console.log('The access token is ' + data.body['access_token']);
-     //console.log('The refresh token is ' + data.body['refresh_token']);
-
-     // Set the access token on the API object to use it in later calls
-     spotifyApi.setAccessToken(data.body['access_token']);
-     spotifyApi.setRefreshToken(data.body['refresh_token']);
-
-     spotifyApi.getMyTopArtists().then(
-       function(data){
-         let topArtists = data.body.items;
-         for(i in topArtists){
-           //console.log(topArtists[i]);
-           topArtistsList += topArtists[i].name +' ';
-         }
-         //console.log(topArtists);
-         console.log("User top artists: ", topArtistsList);
-       }, function (err){
-         console.log("error getting user top artists: ", err);
-       }
-     );
-   },
-   function(err) {
-     console.log('Something went wrong!', err);
-   }
-  );
-  //return topArtistsList;
-}
-//electron.app.commandLine.appendSwitch('host-rules', 'MAP * 127.0.0.1');
 electron.app.on("ready", () => {
   createMainWindow();
 
@@ -114,4 +54,9 @@ electron.app.on("ready", () => {
       createMainWindow();
     }
   });
+/*
+  electron.app.on('window-all-closed', () => {
+    app.quit();
+  });
+*/
 });
